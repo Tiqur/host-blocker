2e5a8a6d73cea62d99f0759f686d6f26c7f1682a
Tiqur <trevorbrage02@gmail.com>
2023-04-24 21:10:26 -0700

    Added user input via CLI

--- main.sh
+++ main.sh
@@ -2,14 +2,37 @@
 
 case "$1" in
   start)
+    # If already running, exit
+    if grep -q "#-HostBlocker-#" /etc/hosts; then
+      echo "Already running..."
+      exit 0
+    fi
+
+    # Take user input until user submits empty line
+    echo "Enter the hosts you want to block:"
+    hosts="\n"
+    while true
+    do
+      read line
+      if [ -z "$line" ]; then
+        break
+      else
+        # Append to hosts string and redirect domain name to localhost
+        hosts+="127.0.0.0 $line"$'\n'
+      fi
+    done
+
+    # Append to hosts file
     echo "Starting..."
-    echo -e "#-HostBlocker-#\ntext\n#-HostBlocker-#" >> /etc/hosts
+    printf  "#-HostBlocker-#%b#-HostBlocker-#" "$hosts" >> /etc/hosts
     ;;
   stop)
+    # Remove from hosts file
     echo "Stopping..."
     sudo sed -i '/#-HostBlocker-#/,/#-HostBlocker-#/d' /etc/hosts
     ;;
   *)
+    # Help
     echo "Usage: $0 {start|stop}"
     exit 1
 esac
